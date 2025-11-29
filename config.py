64c5ceec0dc9a33111cfa8a0e051ec43e8702d28
AshwinBeryl <ashwinberyl@gmail.com>
2025-11-29 08:38:54 +0530

    added a dummy token to test

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     # GitHub OAuth Settings
     GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID')
     GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET')
-    GITHUB_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+    GITHUB_PASS = "mypassword@12345"
     GITHUB_AUTHORIZATION_BASE_URL = 'https://github.com/login/oauth/authorize'
     GITHUB_TOKEN_URL = 'https://github.com/login/oauth/access_token'
     GITHUB_USER_API_URL = 'https://api.github.com/user'
